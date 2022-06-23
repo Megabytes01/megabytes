12e0139315207bd44bc7e591f3088f681f318de7
Megabytes <megabytes@netcabo.pt>
2022-06-23 02:50:42 +0200

    20220622

---
+++ config/config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('ENVIRONMENT', 'development');                  //production | development
+define('BASE_URL',                                                      'http://localhost/megabytes/public/');
